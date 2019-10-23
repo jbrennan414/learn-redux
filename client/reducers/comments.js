10bb1b6d0c734bbd6d7d5127a2a135b0ecacdde6
John Brennan <jpb55116@gmail.com>
2019-10-22 18:17:30 -0600

    wire up redux dev tools and fix remove comment action

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -2,7 +2,39 @@
 // 1) the action (what happenned)
 // 2) a copy of current state
 
+function postComments(state=[], action){
+    switch (action.type) {
+        case 'ADD_COMMENT':
+            //return the new state with a new comment 
+            return [...state, {
+                user:action.author,
+                text:action.comment
+            }];
+
+        case 'REMOVE_COMMENT':
+            return [
+                ...state.slice(0, action.i),
+                ...state.slice(action.i+ 1),
+            ]
+            return state;
+    
+        default:
+            return state;
+    }
+
+    return state;
+}
+
 function comments(state=[], action){
+    if (typeof action.postId !== 'undefined'){
+        return {
+            ///take current state
+            ...state,
+            // overwrite this with a new one
+            [action.postId]: postComments(state[action.postId], action)
+        }
+    }
+
     return state;
 }
 
